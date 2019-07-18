481a8396f6e6dae6653e3d9b2f7cc9b2d9ab616b
Sahar Adem <sahar.adem95@gmail.com>
2019-07-18 08:48:18 +0300

    solving conflict

--- src/handler.js
+++ src/handler.js
@@ -49,9 +49,9 @@ const publicHandler = (request, response, endpoint) => {
 
 const apiHandler = (req, res, endpoint) => {
   const queryString = endpoint.split("=")[1];
+  // console.log("query", queryString
   const decodedInput = decodeURI(queryString).toLowerCase();
-  console.log("query", decodedInput);
-  const url = `https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inl1c3JhLmxhaGFsZWVoQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNTQxNSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOS0wNy0xNiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTYzMzc2Nzc4LCJuYmYiOjE1NjMzNjk1Nzh9.On52MBrJQ7CNgWhTILrNfDqGjeZPhoRZVvXoqRXOG2k&format=json&language=en-gb`;
+  const url = `https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNhaGFyLmFkZW05NUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU0MTQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTktMDctMTYiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU2MzQzNTY0OSwibmJmIjoxNTYzNDI4NDQ5fQ.xhbZBKny0du36U_gWNfWpSfBeZOry5rVmInUOyczk_A&format=json&language=en-gb`;
   request(url, (error, response, body) => {
     if (error) {
       res.writeHead(404, {
@@ -71,13 +71,11 @@ const apiHandler = (req, res, endpoint) => {
       }
     });
     console.log(disease);
-
     let ID = disease[0].ID;
-
     // console.log("ID", ID);
-    const url2 = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[${ID}]&gender=male&year_of_birth=1988&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inl1c3JhLmxhaGFsZWVoQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNTQxNSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOS0wNy0xNiIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTYzMzc2OTE2LCJuYmYiOjE1NjMzNjk3MTZ9.hA0qyh4y_Eni_HeOGwZICdmmRfO4n8wz-3AqVYbcFtw&format=json&language=en-gb`;
+    const url2 = `https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[${ID}]&gender=male&year_of_birth=1988&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNhaGFyLmFkZW05NUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU0MTQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTktMDctMTYiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU2MzQzNTcwOSwibmJmIjoxNTYzNDI4NTA5fQ.n_veH4nwuGNz8S9mmxPyLOk9VwQB9ggNeGvN6ORIBYI&format=json&language=en-gb`;
 
-    request(url2, (err, resp, bod) => {
+    request(url2, (err, resp, body) => {
       if (err) {
         res.writeHead(404, {
           "Content-Type": "text/html"
@@ -89,10 +87,13 @@ const apiHandler = (req, res, endpoint) => {
         "Content-Type": "application/json"
       });
 
-      const diagnosis = JSON.parse(bod);
+      const diagnosis = JSON.parse(body);
 
       const filterDiagnosis = diagnosis.map(element => {
-        return element.Issue.Name;
+        const obj = {};
+        obj.id = element.Issue.ID;
+        obj.name = element.Issue.Name;
+        return obj;
       });
 
       console.log("kgasfd", filterDiagnosis);
@@ -101,8 +102,42 @@ const apiHandler = (req, res, endpoint) => {
   });
 };
 
+const treatmentHandler = (req, res, endpoint) => {
+  //  const treatmentUrl
+  console.log("endpoint", endpoint);
+  const treatmentId = endpoint.split("=")[1];
+  console.log("treatment id", treatmentId);
+  const treatmentUrl = `https://sandbox-healthservice.priaid.ch/issues/${treatmentId}/info?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNhaGFyLmFkZW05NUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU0MTQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTktMDctMTYiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU2MzQzNTc2MywibmJmIjoxNTYzNDI4NTYzfQ.jasl-Tm9Zlgj5dXlQ4GBO78kI4yswtXCzZ9ir2eU_yA&format=json&language=en-gb`;
+  request(treatmentUrl, (err, resp, body) => {
+    if (err) {
+      res.writeHead(404, {
+        "Content-Type": "text/html"
+      });
+      res.end("<h1>Try again</h1>");
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    let treatmentObj = {};
+    treatmentObj.Description = JSON.parse(body).DescriptionShort;
+    treatmentObj.Treatment = JSON.parse(body).TreatmentDescription;
+    treatmentObj.Symptoms = JSON.parse(body).PossibleSymptoms;
+
+    // const filterTreatment = treatmentDescreption.map((element) => {
+
+    // 	return element.;
+    // });
+
+    console.log("treatment", treatmentObj);
+    console.log(JSON.stringify(treatmentObj));
+    res.end(JSON.stringify(treatmentObj));
+  });
+};
+
 module.exports = {
   homeHandler,
   publicHandler,
-  apiHandler
+  apiHandler,
+  treatmentHandler
 };
